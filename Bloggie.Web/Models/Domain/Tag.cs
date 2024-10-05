2f0390189822d8f8acdd41eaf4be12eedc43ec82
NhatAnh <ltnanh21@gmail.com>
2024-10-05 12:37:18 +0700

    Create domain class and add EF Core dependency

---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BlogPostId { get; set; }
+    }
+}
