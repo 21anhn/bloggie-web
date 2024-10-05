087861ddd4a3b81feeefdc8c3420ff57df48024e
NhatAnh <ltnanh21@gmail.com>
2024-10-05 12:46:34 +0700

    Add db context to IoC container

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,3 +1,7 @@
+using Bloggie.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace Bloggie.Web
 {
     public class Program
@@ -8,6 +12,8 @@ namespace Bloggie.Web
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<BloggieDbContext>(options => options.
+            UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
 
             var app = builder.Build();
 
