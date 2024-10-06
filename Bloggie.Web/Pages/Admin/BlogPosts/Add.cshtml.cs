0c2e214cc0566dbf3304bd52b9f8e2b0a1bdf5f3
NhatAnh <ltnanh21@gmail.com>
2024-10-06 13:07:18 +0700

    Add blog post to database by using DbContext

--- Bloggie.Web/Pages/Admin/BlogPosts/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/BlogPosts/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,9 +10,28 @@ namespace Bloggie.Web.Pages.Admin.BlogPosts
     {
         [BindProperty]
         public AddBlogPost? AddBlogPost { get; set; } //ViewModel - get data from view
+        private readonly BloggieDbContext _context;
+
+        public AddModel(BloggieDbContext context)
+        {
+            _context = context;
+        }
         public void OnPost() //Create post
         {
-
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPost.Heading,
+                PageTitle = AddBlogPost.PageTitle,
+                Content = AddBlogPost.Content,
+                ShortDescription = AddBlogPost.ShortDescription,
+                FeaturedImageUrl = AddBlogPost.FeaturedImageUrl,
+                UrlHandle = AddBlogPost.UrlHandle,
+                Author = AddBlogPost.Author,
+                PublishedDate = AddBlogPost.PublishedDate,
+                Visible = AddBlogPost.Visible
+            };
+            _context.BlogPosts.Add(blogPost);
+            _context.SaveChanges();
         }
     }
 }
