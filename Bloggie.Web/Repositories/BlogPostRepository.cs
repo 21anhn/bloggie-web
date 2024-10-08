39ad541273fccb3476e2b066df084e17c4554764
NhatAnh <ltnanh21@gmail.com>
2024-10-08 15:28:02 +0700

    Apply repository pattern

---
+++ Bloggie.Web/Repositories/BlogPostRepository.cs
@@ -0,0 +1,63 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Repositories
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly BloggieDbContext _context;
+
+        public BlogPostRepository(BloggieDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BlogPost> AddAsync(BlogPost blogPost)
+        {
+            await _context.BlogPosts.AddAsync(blogPost);
+            await _context.SaveChangesAsync();
+            return blogPost;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var existingBlogPost = await _context.BlogPosts.FindAsync(id);
+            if (existingBlogPost != null)
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<BlogPost>> GetAllAsync()
+        {
+            return await _context.BlogPosts.ToListAsync();
+        }
+
+        public async Task<BlogPost> GetAsync(int id)
+        {
+            return await _context.BlogPosts.FindAsync(id);
+        }
+
+        public async Task<BlogPost> UpdateAsync(BlogPost blogPost)
+        {
+            var existingBlogPost = await _context.BlogPosts.FindAsync(blogPost.Id);
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Heading = blogPost.Heading;
+                existingBlogPost.PageTitle = blogPost.PageTitle;
+                existingBlogPost.Content = blogPost.Content;
+                existingBlogPost.ShortDescription = blogPost.ShortDescription;
+                existingBlogPost.FeaturedImageUrl = blogPost.FeaturedImageUrl;
+                existingBlogPost.UrlHandle = blogPost.UrlHandle;
+                existingBlogPost.Author = blogPost.Author;
+                existingBlogPost.PublishedDate = blogPost.PublishedDate;
+                existingBlogPost.Visible = blogPost.Visible;
+            }
+            await _context.SaveChangesAsync();
+            return existingBlogPost;
+        }
+    }
+}
