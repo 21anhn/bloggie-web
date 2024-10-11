346834220c41d8b0db7eb075a645eeb5fb9c0166
NhatAnh <ltnanh21@gmail.com>
2024-10-11 09:43:36 +0700

    Displaying blogs in home page

--- Bloggie.Web/Pages/Index.cshtml.cs
+++ Bloggie.Web/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +8,19 @@ namespace Bloggie.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IBlogPostRepository _repository;
+        public List<BlogPost> BlogPosts { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IBlogPostRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            BlogPosts = (await _repository.GetAllAsync()).ToList();
+            return Page();
         }
     }
 }
