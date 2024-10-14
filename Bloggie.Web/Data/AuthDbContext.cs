9671f70bb48f6708fe9a80793d26c75e84060175
NhatAnh <ltnanh21@gmail.com>
2024-10-14 08:06:46 +0700

    Add database migrations for AuthDb

--- Bloggie.Web/Data/AuthDbContext.cs
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -73,8 +73,8 @@ namespace Bloggie.Web.Data
                 },
                 new IdentityUserRole<string>
                 {
-                    RoleId = adminRoleId,
-                    UserId = userRoleId
+                    RoleId = userRoleId,
+                    UserId = superAdminId
                 }
             };
 
