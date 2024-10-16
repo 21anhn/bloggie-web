a595dc917808435018f867f5941c9b0469181146
NhatAnh <ltnanh21@gmail.com>
2024-10-16 18:40:03 +0700

    Domain model for blog post like and migration

---
+++ Bloggie.Web/Models/Domain/BlogPostLike.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPostLike
+    {
+        public int Id { get; set; }
+        public int BlogPostId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
