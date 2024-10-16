4c54ac2d7c5d8fbef8ae0b145fc3d314146bd0c6
NhatAnh <ltnanh21@gmail.com>
2024-10-16 15:39:30 +0700

    Updata seed data

--- Bloggie.Web/Data/AuthDbContext.cs
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -52,7 +52,9 @@ namespace Bloggie.Web.Data
             {
                 Id = superAdminId,
                 UserName = "superadmin@bloggie.com",
-                Email = "superadmin@bloggie.com"
+                Email = "superadmin@bloggie.com",
+                NormalizedEmail = "superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superadmin@bloggie.com".ToUpper()
             };
 
             superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "superadmin123");
---
+++ Bloggie.Web/Migrations/AuthDb/20241016083744_Adding normalized username and email.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class Addingnormalizedusernameandemail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6c2217bb-c600-435f-90df-10df7552663e",
+                columns: new[] { "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7b03fd4c-a9d1-4fc1-83fb-ea959e3e77c0", "SUPERADMIN@BLOGGIE.COM", "SUPERADMIN@BLOGGIE.COM", "AQAAAAIAAYagAAAAEOZq9wDwf2cG3zv4tIxB++kmo/1AFOSCAYpH+r8tbTr3T+0WsN91TRYKGrzwmLqIRg==", "0dae6a7a-c39f-4a8c-b850-516a7dd11076" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "6c2217bb-c600-435f-90df-10df7552663e",
+                columns: new[] { "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "fe982762-2734-4225-8815-c04ef424ba8a", null, null, "AQAAAAIAAYagAAAAEJ9cd9EY20wo9txO41WDD02+CtbwhJzZB7pXynh07JRGmchgYjejO9qV1BkSe9QrJw==", "0546e6c1-f50c-4f95-98ba-e00b0265076d" });
+        }
+    }
+}
