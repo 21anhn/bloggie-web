f390f4eb85720598842c42b01d3c61a9f217892f
NhatAnh <ltnanh21@gmail.com>
2024-10-21 10:19:41 +0700

    Update DbSet name

---
+++ Bloggie.Web/Migrations/20241021031836_Update name DbSet.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatenameDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_BlogPosts_BlogPostId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "Posts",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Posts_BlogPostId",
+                table: "Comments",
+                newName: "IX_Comments_BlogPostId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_BlogPosts_BlogPostId",
+                table: "Comments",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_BlogPosts_BlogPostId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Posts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_BlogPostId",
+                table: "Posts",
+                newName: "IX_Posts_BlogPostId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_BlogPosts_BlogPostId",
+                table: "Posts",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
